ad3bd37d630b9a9485c8ad8cd0548cd62c2544e4
Kato Masato <masato.kato0604@gmail.com>
2023-01-03 01:43:51 +0900

    Remove signal handling and add connection handling method.
    Added connection handling methods to read HTTP requests.
    Signal handling was removed temporarily because it would not work well with the connection handling methods.

--- examples/sample.rs
+++ examples/sample.rs
@@ -5,5 +5,5 @@ fn main() -> std::io::Result<()> {
     // Shs::new(
     //
     // ).bind()?.run()
-    Server::new().run()
+    Server::bind(("127.0.0.1", 8080)).run()
 }
--- src/server.rs
+++ src/server.rs
@@ -1,37 +1,53 @@
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::{fmt, io, thread, time};
+use std::{fmt, io, net, thread, time};
+use std::net::{TcpStream, TcpListener};
+use std::io::prelude::*;
 
 use signal_hook::consts::TERM_SIGNALS;
 use signal_hook::flag;
 
-pub struct Server {}
+pub struct Server {
+    listener: TcpListener,
+}
 
 impl Server {
-    pub fn new() -> Self {
-        Server {}
+    pub fn bind<A: net::ToSocketAddrs>(addrs: A) -> Self {
+        let listener = TcpListener::bind(addrs).unwrap();
+        Server {
+            listener,
+        }
     }
 
-    pub fn run(self) -> Result<(), io::Error> {
+    pub fn run(&self) -> Result<(), io::Error> {
         println!("shs HTTP server start!");
-        let n = 0;
-        let term = Arc::new(AtomicBool::new(false));
+        // let term = Arc::new(AtomicBool::new(false));
+        //
+        // for sig in TERM_SIGNALS {
+        //     flag::register_conditional_shutdown(*sig, 1, Arc::clone(&term))?;
+        //     flag::register(*sig, Arc::clone(&term))?;
+        // }
 
-        for sig in TERM_SIGNALS {
-            flag::register_conditional_shutdown(*sig, 1, Arc::clone(&term))?;
-            flag::register(*sig, Arc::clone(&term))?;
+        for stream in self.listener.incoming() {
+            let stream = stream.unwrap();
+            self.handle_connection(stream);
         }
-
-        while !term.load(Ordering::Relaxed) {
-            println!("http server working...");
-            thread::sleep(time::Duration::from_secs(1));
-        }
-        println!(
-            "\nReceived kill signal. Wait 10 seconds, or hit Ctrl+C again to exit immediately."
-        );
-        thread::sleep(time::Duration::from_secs(1));
+        // while !term.load(Ordering::Relaxed) {
+        //     println!("working...");
+        //     thread::sleep(time::Duration::from_secs(1));
+        // }
+        // println!(
+        //     "\nReceived kill signal. Wait 10 seconds, or hit Ctrl+C again to exit immediately."
+        // );
+        // thread::sleep(time::Duration::from_secs(1));
 
         println!("Exited cleanly");
         Ok(())
+    }
+
+    fn handle_connection(&self, mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
     }
 }
