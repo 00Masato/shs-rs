af2715c7c0a129a570f2c1828d4afe8db7221723
Kato Masato <masato.kato0604@gmail.com>
2023-01-05 23:09:28 +0900

    Add 404 response and refactoring.
    Added 404 response and refactored response headers and response file

--- src/server.rs
+++ src/server.rs
@@ -48,11 +48,21 @@ impl Server {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
 
-        let mut file = File::open("index.html").unwrap();
+        let get = b"GET / HTTP/1.1\r\n";
 
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+
+        let mut file = File::open(filename).unwrap();
         let mut contents = String::new();
+
         file.read_to_string(&mut contents).unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        let response = format!("{}{}", status_line, contents);
+
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
         println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
